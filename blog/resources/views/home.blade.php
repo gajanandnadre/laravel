e2e3c6b0d110e056e167b9dfb1e1b1645ef8e602
gajanand <nadregaju2019@gmail.com>
2025-03-27 09:36:22 +0530

    route pratice

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,1 +1,4 @@
 <h1> This is home page  </h1>
+
+<a href="/">welcome</a>
+<a href="/about/gaju">About</a>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/home', function () {
-//     return view('home');
-// });
-Route::view("/home","home");
+Route::get('/home', function () {
+    return view('home');
+});
+// Route::view("/home","home");
+
+Route::get('/about/{name}',function($name){
+    
+    return view('about',['name'=>$name]);
+});
